8e88735ce0bce2be060b4480d3819e50f4e8d92a
Jan-Nr5 <macbulldon4444@gmail.com>
2026-02-12 09:48:55 +0100

    Added new Procedures

--- DataModification.sql
+++ DataModification.sql
@@ -102,3 +102,172 @@ CALL person_anlegen(
     NULL    -- Nur bei Mitarbeitern
 );
 
+------------------------------
+--
+-- Buche Termin
+--
+------------------------------
+
+DELIMITER $$
+
+CREATE PROCEDURE termin_buchen (
+    IN p_mitglied_id INT,
+    IN p_termin_id INT
+)
+BEGIN
+
+    DECLARE v_kurs_id INT;
+    DECLARE v_tarif_id INT;
+    DECLARE v_kapazitaet INT;
+    DECLARE v_teilnehmer INT;
+
+    START TRANSACTION;
+
+    -- Kurs zum Termin holen
+    SELECT kurs_id
+    INTO v_kurs_id
+    FROM termin
+    WHERE termin_id = p_termin_id
+    FOR UPDATE; -- Damit nicht mehrere Buchungen gleixhzeitig passieren
+
+    SELECT tarif_id
+    INTO v_tarif_id
+    FROM mitgliedschaft
+    WHERE mitglied_id = p_mitglied_id
+      AND status = 'aktiv'
+      AND (`enddatum` IS NULL OR `enddatum` >= CURRENT_DATE)
+    LIMIT 1;
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM tarif_kurs
+        WHERE tarif_id = v_tarif_id
+          AND kurs_id = v_kurs_id
+    ) THEN
+        SIGNAL SQLSTATE '45000' -- User Defined Err
+        SET MESSAGE_TEXT = 'Tarif erlaubt diesen Kurs nicht';
+    END IF;
+
+    SELECT r.kapazitaet
+    INTO v_kapazitaet
+    FROM termin t
+    JOIN raum r ON t.raum_id = r.raum_id
+    WHERE t.termin_id = p_termin_id
+    FOR UPDATE;
+
+    SELECT COUNT(*)
+    INTO v_teilnehmer
+    FROM termin_teilnahme
+    WHERE termin_id = p_termin_id
+      AND status IN ('gebucht','besucht');
+
+    IF v_teilnehmer >= v_kapazitaet THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Termin ist ausgebucht';
+    END IF;
+
+    INSERT INTO termin_teilnahme (
+        termin_id,
+        mitglied_id
+    )
+    VALUES (
+        p_termin_id,
+        p_mitglied_id
+    );
+
+    COMMIT;
+
+END$$
+
+DELIMITER ;
+
+----------------------------
+--
+-- Stoniere gebuchten Termin
+--
+----------------------------
+
+DELIMITER $$
+
+CREATE PROCEDURE termin_stornieren (
+    IN p_mitglied_id INT,
+    IN p_termin_id INT
+)
+BEGIN
+
+    START TRANSACTION;
+
+    UPDATE termin_teilnahme
+    SET status = 'storniert'
+    WHERE mitglied_id = p_mitglied_id
+      AND termin_id = p_termin_id;
+
+    COMMIT;
+
+END$$
+
+DELIMITER ;
+
+--------------------------
+--
+-- Automatischer Checkin
+--
+--------------------------
+DELIMITER $$
+
+CREATE PROCEDURE checkin_durchfuehren (
+    IN p_mitglied_id INT,
+    IN p_termin_id INT
+)
+BEGIN
+
+    START TRANSACTION;
+
+    INSERT INTO checkin (
+        mitglied_id,
+        termin_id
+    )
+    VALUES (
+        p_mitglied_id,
+        p_termin_id
+    );
+
+    UPDATE termin_teilnahme
+    SET status = 'besucht'
+    WHERE mitglied_id = p_mitglied_id
+      AND termin_id = p_termin_id;
+
+    COMMIT;
+
+END$$
+
+DELIMITER ;
+
+---------------------
+--
+-- Automatischer Checkout
+--
+----------------------
+DELIMITER $$
+
+CREATE PROCEDURE checkout_durchfuehren (
+    IN p_mitglied_id INT,
+    IN p_termin_id INT
+)
+BEGIN
+
+    START TRANSACTION;
+
+    UPDATE checkin
+    SET checkout_zeit = CURRENT_TIMESTAMP
+    WHERE mitglied_id = p_mitglied_id
+      AND termin_id = p_termin_id
+      AND checkout_zeit IS NULL;
+
+    COMMIT;
+
+END$$
+
+DELIMITER ;
+
+
